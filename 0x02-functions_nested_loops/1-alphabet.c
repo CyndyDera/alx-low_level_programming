19c7e4b3cfca14873a89f6ac0f0e5bc908532955
Cynthia Okoye <cyndyokoye252@gmail.com>
2023-02-21 07:20:25 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet - prints all alphabets in lowercase
+ */
+void print_alphabet(void);
+{
+char let;
+for (let = 'a', let <= 'z', let++)
+_putchar(let);
+_putchar('\n');
+}
