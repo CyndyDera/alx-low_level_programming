2d2aea31b3357d7ae67f2c1ce371e16e9486c46f
Cynthia Okoye <cyndyokoye252@gmail.com>
2023-02-21 16:04:30 -0800

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,8 @@
 /**
  * print_sign - Program that prints the sign of a number
  * @n: The int to check
- * Return: 1 print + if n > 0, 0 prnt 0, 1 print -
+ * Return: 1 printand print + if n greater than zero
+ * 0 and print 0 if
  */
 int print_sign(int n)
 {
@@ -13,7 +14,7 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar(48);
 		return (0);
 	}
 	else
@@ -21,5 +22,4 @@ int print_sign(int n)
 		_putchar('-');
 		return (-1);
 	}
-	return
 }
