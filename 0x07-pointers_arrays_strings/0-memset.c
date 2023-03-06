ca598863523851be5b4307aa9705ee9287858d47
Cynthia Okoye <cyndyokoye252@gmail.com>
2023-03-06 05:19:42 -0800

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - function that fills memory with the constant byte @b
+ * @n: bytes of the memory area pointed to by @s
+ * @s: with the constant byte @b
+ * @b: memory area pointer
+ * Return: a pointer to the memory area @s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int t = 0;
+
+	while (t < n)
+	{
+	s[t] = b;
+	t++;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,8 @@
+# 0x07-pointers_arrays_strings
+
+## Even more on pointers
+
+* What are pointers to pointers and how to use them
+* What are multidimensional arrays and how to use them
+* What are the most common C standard library functions to manipulate strings
+* Two Dimensional (2D) arrays in C.
