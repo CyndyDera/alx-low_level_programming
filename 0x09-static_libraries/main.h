8d67ef8c208aa6a6cb1ab13206ac57e1bf1cf8df
Cynthia Okoye <cyndyokoye252@gmail.com>
2023-03-12 03:32:43 -0700

    Stressed task

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stddef.h>
+
+/*
+ * File: main.h
+ * Auth: Tksmith
+ * Desc: This is is a headaer file that contaisn all the prototypes
+ * to be used in the c-program
+ */
 
 int _putchar(char c);
 int _islower(int c);
@@ -20,7 +26,7 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+char *_strpbrk(char *s, char *accept);
 
 #endif
